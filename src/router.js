3e2668d913f2be3bafae9eec684acac4fa8a6b13
websir <zjpzjp@foxmail.com>
2018-07-25 10:14:41 +0800

    -

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import user from "@/router/user.js";
+import vehicle from "@/router/user.js";
+import device from "@/router/device.js";
 Vue.use(Router);
 
 export default new Router({
@@ -31,74 +33,9 @@ export default new Router({
       meta: { name: "运营管理" },
       component: () => import("./views/manage/index.vue"),
       children: [
-        {
-          path: "/vehicle",
-          name: "vehicle",
-          meta: { name: "车辆管理", icon: "iconfont icon-car-fill" },
-          component: () => import("./views/manage/vehicle/index.vue"),
-          children: [
-            {
-              path: "new",
-              name: "new",
-              meta: { name: "新增车辆管理" },
-              component: () => import("./views/manage/vehicle/new/index.vue")
-            },
-            {
-              path: "new/add",
-              name: "new-add",
-              meta: { name: "新增车辆管理-添加", hidden: true },
-              component: () => import("./views/manage/vehicle/new/add.vue")
-            },
-            {
-              path: "position",
-              name: "position",
-              meta: { name: "新增车辆管理" },
-              component: () => import("./views/manage/vehicle/new/index.vue")
-            }
-          ]
-        },
-        {
-          path: "device",
-          name: "device",
-          meta: { name: "终端管理", icon: "iconfont icon-boxplot-fill" },
-          component: () => import("./views/manage/device/index.vue"),
-          children: [
-            {
-              path: "device",
-              name: "device-device",
-              meta: { name: "设备管理", icon: "iconfont icon-boxplot-fill" },
-              component: () => import("./views/manage/device/device/index.vue")
-            },
-            {
-              path: "device",
-              name: "device-repair",
-              meta: {
-                name: "维修设备管理",
-                icon: "iconfont icon-wrench-fill"
-              },
-              component: () => import("./views/manage/device/repair/index.vue")
-            },
-            {
-              path: "device",
-              name: "device-sim",
-              meta: {
-                name: "SIM卡管理",
-                icon: "iconfont icon-boxplot-fill"
-              },
-              component: () => import("./views/manage/device/sim/index.vue")
-            },
-            {
-              path: "device",
-              name: "device-company",
-              meta: {
-                name: "终端厂商管理",
-                icon: "iconfont icon-boxplot-fill"
-              },
-              component: () => import("./views/manage/device/company/index.vue")
-            }
-          ]
-        },
+        vehicle,
         user,
+        device,
         {
           path: "vehicle-setting",
           name: "vehicle-setting",
