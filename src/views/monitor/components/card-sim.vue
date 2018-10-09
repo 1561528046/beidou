3b5d469f43f3699a8fe3857da9361afdf6ea098b
zhaoxiangyu <zhaoxiangyu@cccts.com>
2018-10-09 17:04:19 +0800

    -

--- src/views/monitor/components/card-sim.vue
+++ src/views/monitor/components/card-sim.vue
@@ -1,59 +1,66 @@
 <template slot-scope="scope">
-    <div style="width:40%;">
-        <el-form :model="tableData" size="small" ref="baseForm">
-            <el-card shadow="hover">
-                <div slot="header" class="clearfix">
-                    <span style="font-size:16px">SIM卡信息</span>
-                </div>
-                <el-row :gutter="30">
-                    <el-col :span="8">
-                        <el-form-item label="SIM卡号" prop="sim_no">
-                        </el-form-item>
-                    </el-col>
-                    <el-col :span="8">
-                        <el-form-item label="ICCID" prop="camera_num">
-                        </el-form-item>
-                    </el-col>
-                    <el-col :span="8">
-                        <el-form-item label="所属运营商" prop="belong">
-                        </el-form-item>
-                    </el-col>
-                    <el-col :span="8">
-                        <el-form-item label="流量" prop="flow">
-                        </el-form-item>
-                    </el-col>
-                    <el-col :span="8">
-                        <el-form-item label="备注" prop="note">
-                        </el-form-item>
-                    </el-col>
-                </el-row>
-            </el-card>
+    <div>
+        <el-form :model="formData" size="small">
+            <div slot="header" class="clearfix">
+                <span style="font-size:16px">SIM卡信息</span>
+            </div>
+            <el-row :gutter="30">
+                <el-col :span="8">
+                    <el-form-item label="SIM卡号：">
+                        <label>{{formData.sim_no}}</label>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="8">
+                    <el-form-item label="ICCID：">
+                        <label>{{formData.icc_id}}</label>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="8">
+                    <el-form-item label="所属运营商：">
+                        <label>{{formData.belong}}</label>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="8">
+                    <el-form-item label="流量：">
+                        <label>{{formData.flow}}</label>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="8">
+                    <el-form-item label="备注：">
+                        <label>{{formData.note}}</label>
+                    </el-form-item>
+                </el-col>
+            </el-row>
         </el-form>
     </div>
 </template>
 <script>
-// import { getSim } from "@/api/index.js";
+import { getSim } from "@/api/index.js";
 export default {
   created() {
     this.getTable();
   },
   data() {
     return {
-      tableData: {}
+      sim_id: "",
+      formData: {}
     };
   },
-  props: {
-    sim_id: String
-  },
+  props: ["vehicle"],
   methods: {
     getTable() {
-      //   getSim({ sim_id: this.$props.sim_id }).then(res => {
-      //     if (res.data.code == 0) {
-      //       this.$set(this.$data, "tableData", res.data.data);
-      //     } else {
-      //       this.$message.error(res.data.msg);
-      //     }
-      //   });
+      this.$set(this.$data, "sim_id", this.$props.vehicle.sim_id);
+      getSim({ id: this.sim_id }).then(res => {
+        if (res.data.code == 0) {
+          if (res.data.data.length == 0) {
+            this.$set(this.$data, "formData", {});
+          } else {
+            this.$set(this.$data, "formData", res.data.data[0]);
+          }
+        } else {
+          this.$message.error(res.data.msg);
+        }
+      });
     }
   }
 };
