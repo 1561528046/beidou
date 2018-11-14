7f54c7f751a98b9e0599859ade7d7f154ffd11e2
websir <zjpzjp@foxmail.com>
2018-11-14 09:22:52 +0800

    -

--- src/utils/base.js
+++ src/utils/base.js
@@ -1,11 +1,11 @@
-// var BASE_URL = "http://192.168.88.88:10462/";
-// var API_URL = "http://192.168.88.88:10462/api";
-// var MONITOR_URL = "ws://192.168.88.88:5002";
-// var INSTRUCTION_URL = "ws://192.168.88.88:5000";
-var BASE_URL = "http://60.10.139.113:1023/";
-var API_URL = "http://60.10.139.113:1023/api";
-var INSTRUCTION_URL = "ws://60.10.139.112:5000";
-var MONITOR_URL = "ws://60.10.139.112:5002";
+var BASE_URL = "http://192.168.88.100:725/";
+var API_URL = "http://192.168.88.100:725/api";
+var MONITOR_URL = "ws://192.168.88.125:5002";
+var INSTRUCTION_URL = "ws://192.168.88.125:5000";
+// var BASE_URL = "http://60.10.139.113:1023/";
+// var API_URL = "http://60.10.139.113:1023/api";
+// var INSTRUCTION_URL = "ws://60.10.139.112:5000";
+// var MONITOR_URL = "ws://60.10.139.112:5002";
 
 // || true
 if (process.env.NODE_ENV == "production") {
