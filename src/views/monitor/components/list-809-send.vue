f3b6ce08f9c2bcb6a134d6827cdcf56f2f50ad61
zhaoxiangyu <zhaoxiangyu@cccts.com>
2018-11-16 10:11:00 +0800

    -

---
+++ src/views/monitor/components/list-809-send.vue
@@ -0,0 +1,26 @@
+<template>
+    <div style="padding:10px 10px;">
+        <label style="font-size:14px;">发送类型：</label>
+        <el-select style="width:20%;" size="small" v-model="send">
+            <el-option value="1" label="申请交换指定车辆定位信息请求消息">申请交换指定车辆定位信息请求消息</el-option>
+            <el-option value="2" label="取消交换指定车辆定位信息请求消息">取消交换指定车辆定位信息请求消息</el-option>
+            <el-option value="3" label="补发车辆定位信息请求">补发车辆定位信息请求</el-option>
+            <el-option value="4" label="主动上报驾驶员身份信息">主动上报驾驶员身份信息</el-option>
+            <el-option value="5" label="主动上报车辆电子运单信息">主动上报车辆电子运单信息</el-option>
+            <el-option value="6" label="平台查岗应答">平台查岗应答</el-option>
+            <el-option value="7" label="报警督办应答">报警督办应答</el-option>
+            <el-option value="8" label="上报报警信息">上报报警信息</el-option>
+            <el-option value="9" label="主动上报报警处理结果">主动上报报警处理结果</el-option>
+            <el-option value="10" label="补报车辆静态信息请求消息">补报车辆静态信息请求消息</el-option>
+        </el-select>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      send: ""
+    };
+  }
+};
+</script>
--- src/views/monitor/components/monitor-info.vue
+++ src/views/monitor/components/monitor-info.vue
@@ -6,36 +6,58 @@
     </div>
     <el-tabs v-model="currentTab" type="border-card" @tab-click="smallSize = false">
       <el-tab-pane :closable="false" name="QA">
-        <span slot="label"> 提问列表<el-badge :value="$store.getters['QA/askList'].length" :max="99" class="monitor-badge"> </el-badge> </span>
+        <span slot="label"> 提问列表
+          <el-badge :value="$store.getters['QA/askList'].length" :max="99" class="monitor-badge"> </el-badge>
+        </span>
         <qa-list></qa-list>
       </el-tab-pane>
       <el-tab-pane :closable="false" name="x0701">
-        <span slot="label"> 电子运单<el-badge :value="$store.getters['x0701/list'].length" :max="99" class="monitor-badge"> </el-badge></span>
+        <span slot="label"> 电子运单
+          <el-badge :value="$store.getters['x0701/list'].length" :max="99" class="monitor-badge"> </el-badge>
+        </span>
         <list-x0701></list-x0701>
       </el-tab-pane>
       <el-tab-pane :closable="false" name="x0301">
-        <span slot="label"> 事件报告<el-badge :value="$store.getters['x0301/list'].length" :max="99" class="monitor-badge"> </el-badge></span>
+        <span slot="label"> 事件报告
+          <el-badge :value="$store.getters['x0301/list'].length" :max="99" class="monitor-badge"> </el-badge>
+        </span>
         <list-x0301></list-x0301>
       </el-tab-pane>
       <el-tab-pane :closable="false" name="x8203">
-        <span slot="label"> 报警消息<el-badge :value="$store.getters['alarm/list'].length" :max="99" class="monitor-badge"> </el-badge></span>
+        <span slot="label"> 报警消息
+          <el-badge :value="$store.getters['alarm/list'].length" :max="99" class="monitor-badge"> </el-badge>
+        </span>
         <list-x8203></list-x8203>
       </el-tab-pane>
       <el-tab-pane :closable="false" name="x0900">
-        <span slot="label"> 上行透传<el-badge :value="$store.getters['x0900/list'].length" :max="99" class="monitor-badge"> </el-badge></span>
+        <span slot="label"> 上行透传
+          <el-badge :value="$store.getters['x0900/list'].length" :max="99" class="monitor-badge"> </el-badge>
+        </span>
         <list-x0900></list-x0900>
       </el-tab-pane>
       <el-tab-pane :closable="false" name="x0901">
-        <span slot="label"> 数据压缩上报<el-badge :value="$store.getters['x0901/list'].length" :max="99" class="monitor-badge"> </el-badge></span>
+        <span slot="label"> 数据压缩上报
+          <el-badge :value="$store.getters['x0901/list'].length" :max="99" class="monitor-badge"> </el-badge>
+        </span>
         <list-x0901></list-x0901>
       </el-tab-pane>
       <el-tab-pane :closable="false" name="list809">
-        <span slot="label"> 查岗<el-badge :value="$store.getters['x809/list'].length" :max="99" class="monitor-badge"> </el-badge></span>
+        <span slot="label"> 查岗
+          <el-badge :value="$store.getters['x809/list'].length" :max="99" class="monitor-badge"> </el-badge>
+        </span>
         <list809></list809>
       </el-tab-pane>
       <el-tab-pane :closable="false" name="list809in">
-        <span slot="label"> 890数据交换<el-badge :value="$store.getters['x809In/list'].length" :max="99" class="monitor-badge"> </el-badge></span>
+        <span slot="label"> 809数据交换
+          <el-badge :value="$store.getters['x809In/list'].length" :max="99" class="monitor-badge"> </el-badge>
+        </span>
         <list809-in></list809-in>
+      </el-tab-pane>
+      <el-tab-pane :closable="false" name="list809send">
+        <span slot="label"> 809数据发送
+          <el-badge :max="99" class="monitor-badge"> </el-badge>
+        </span>
+        <list809-send></list809-send>
       </el-tab-pane>
     </el-tabs>
   </div>
@@ -49,6 +71,7 @@ import listX0900 from "./list-x0900.vue"; //数据上行透传
 import listX0901 from "./list-x0901.vue"; //数据压缩上报
 import list809 from "./list-809.vue"; //809查岗
 import list809In from "./list-809-in.vue"; //809查岗
+import list809Send from "./list-809-send.vue"; //809发送
 export default {
   components: {
     qaList,
@@ -58,7 +81,8 @@ export default {
     listX0900,
     listX0901,
     list809,
-    list809In
+    list809In,
+    list809Send
   },
   data() {
     return {
