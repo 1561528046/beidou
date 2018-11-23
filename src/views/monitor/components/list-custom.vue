f1a7c649d693cd344e4725514549285e72197983
websir <zjpzjp@foxmail.com>
2018-11-23 09:32:45 +0800

    修复indexDB版本问题

--- src/views/monitor/components/list-custom.vue
+++ src/views/monitor/components/list-custom.vue
@@ -1,15 +1,11 @@
 <template>
   <div style="padding-top:20px;" class="log-container">
-    <!-- <ul class="log">
+    <ul class="log">
       <li v-for="log in logs" :key="log.id">
         {{log}}
       </li>
-    </ul> -->
+    </ul>
     <div class="send">
-      指令类型：
-      <el-select style="width:40%; margin-bottom:10px;" v-model="instruction" size="small">
-        <el-option v-for="(item,index) in instruction809" :key="index" :value="index" :label="item.title"></el-option>
-      </el-select>
       <el-input style="margin-bottom:10px;" type="textarea" :rows="4" placeholder="请输入内容" v-model="textarea">
       </el-input>
       <el-button style="display:block;margin:0 auto;" @click="send" type="primary" size="small">发送</el-button>
@@ -17,24 +13,6 @@
 
   </div>
 </template>
-<script>
-import { instruction809 } from "@/utils/809.js";
-export default {
-  created() {
-    this.$set(this.$data, "instruction809", instruction809);
-  },
-  data() {
-    return {
-      textarea: "",
-      instruction: "",
-      instruction809: []
-    };
-  },
-  methods: {
-    send() {}
-  }
-};
-</script>
 <style lang="less">
 .log-container {
   .send {
@@ -63,20 +41,34 @@ export default {
 }
 </style>
 <script>
-import { instruction809 } from "@/utils/809.js";
+import db from "@/utils/indexedDB.js";
 export default {
-  created() {
-    this.$set(this.$data, "instruction809", instruction809);
-  },
   data() {
     return {
       textarea: "",
-      instruction: "",
-      instruction809: []
+      logs: []
     };
   },
+  created() {
+    db.then(dbObj => {
+      setInterval(() => {
+        dbObj
+          .readAll()
+          .then(res => {
+            this.$set(this.$data, "logs", res);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }, 1000);
+    }).catch(err => {
+      console.log(err);
+    });
+  },
   methods: {
-    send() {}
+    send() {
+      this.$instruction.send(this.textarea);
+    }
   }
 };
 </script>
